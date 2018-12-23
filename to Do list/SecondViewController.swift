8711ed8fbdee5bce08621c2781d602967a4f0e21
Mac <mac@macs-macbook-pro-2.local>
2018-12-23 18:01:18 +0530

    Initial Commit

---
+++ to Do list/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  to Do list
+//
+//  Created by Mac on 23/12/18.
+//  Copyright © 2018 Mac. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
